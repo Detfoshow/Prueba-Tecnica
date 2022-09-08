0fd1e4ec31541968acc5727537b987e2cc3571ce
Ricardo <ricardo.f.s.gamboa@gmail.com>
2022-09-08 15:43:06 -0300

    Reordenamos las carpetas del proyecto y añadimos Docker

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
+
+class Item(BaseModel):
+    patente: str
+    id1: float
+
+app = FastAPI()
+
+patentes = {
+  1: "AAAA000",
+  2: "AAAA001",
+  3: "AAAA002",
+}
+
+@app.get("/id/{pat_id}")
+def read_id(pat_id: int, ): 
+    p = patentes[pat_id] 
+    return {"pat_id": pat_id, "patente": p}
+
+
+@app.get("/patente/{patente}")
+def read_pat(patente: str):
+    pat = [k for k, v in patentes.items() if v == patente][0]
+    return {"pat_id": pat, "patente": patente}
+
+
+@app.put("/id/{pat_id}")
+def read_pat(patente: str):
+    pat = [k for k, v in patentes.items() if v == patente][0]
+    return {"pat_id": pat, "patente": patente}
